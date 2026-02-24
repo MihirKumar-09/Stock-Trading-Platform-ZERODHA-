db4c95a247bf596c55a8982860f9dcadc6197603
MihirKumar <mihirmalik34@gmail.com>
2026-02-25 01:45:47 +0530

    Design basic home page

--- frontend/src/landing_page/Home/Hero.jsx
+++ frontend/src/landing_page/Home/Hero.jsx
@@ -1,3 +1,20 @@
+import style from "./Hero.module.css";
 export default function Hero() {
-  return <h1>Hero </h1>;
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="Hero Image"
+          className="mb-5"
+        />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivative, mutual funds, and
+          more
+        </p>
+        <button className={style.signUpBtn}>Signup now</button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/landing_page/Home/Stats.jsx
+++ frontend/src/landing_page/Home/Stats.jsx
@@ -1,3 +1,47 @@
 export default function Stats() {
-  return <h1>Stats</h1>;
+  return (
+    <div className="container mt-5">
+      <div className="row p-5">
+        <div className="col-6 p-5">
+          <h1 className="fs-2 mb-5">Trust with confidence</h1>
+          <h2 className="fs-4">Customer-first always</h2>
+          <p className="text-muted mb-4">
+            That's why 1.3+ crore customer trust Zerodha with ₹3.5+ lakh crore
+            wroth of quality investment
+          </p>
+          <h2 className="fs-4">No Spam or gimmicks</h2>
+          <p className="text-muted mb-4">
+            No gimmicks, spam, "gamification", or annoying push notifications.
+            High quality apps that you use at your pace, the way you like.
+          </p>
+          <h2 className="fs-4">The Zerodha universe</h2>
+          <p className="text-muted mb-4">
+            Not just an app, but a whole ecosystem. Our investment in 30+
+            fintech startups offer you tailored service specific to your needs.
+          </p>
+          <h2 className="fs-4">Do better with money</h2>
+          <p className="text-muted mb-4">
+            With initiative like Nudge and Kill Switch, we don't just facilitate
+            transaction, but actively help you do better with your money
+          </p>
+        </div>
+        <div className="col-6 p-5">
+          <img
+            src="media/images/ecosystem.png"
+            alt="Eco-System"
+            style={{ width: "90%" }}
+          />
+          <div className="text-center">
+            <a href="#" className="mx-5" style={{ textDecoration: "none" }}>
+              Explore our products
+              <i className="fa-solid fa-arrow-right-long mx-1"></i>
+            </a>
+            <a href="#" style={{ textDecoration: "none" }}>
+              Try kite demo<i className="fa-solid fa-arrow-right-long mx-1"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
